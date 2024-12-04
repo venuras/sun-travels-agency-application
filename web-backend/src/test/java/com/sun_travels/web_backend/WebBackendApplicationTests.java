294ea0b3f72c182af2e13b979374b9812e5c7e7f
venuras <venuras@codegen.net>
2024-12-04 17:18:45 +0530

    removed unused imports and used Testcontainer configuration to pass contextLoads test

--- web-backend/src/test/java/com/sun_travels/web_backend/WebBackendApplicationTests.java
+++ web-backend/src/test/java/com/sun_travels/web_backend/WebBackendApplicationTests.java
@@ -1,17 +1,10 @@
 package com.sun_travels.web_backend;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@ContextConfiguration
-@ActiveProfiles(value = "test")
-class WebBackendApplicationTests {
+class WebBackendApplicationTests extends TestContainersConfiguration{
 
 	@Test
 	void contextLoads() {
