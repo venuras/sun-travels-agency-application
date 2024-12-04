83b1ce25e0849f7c3bc457268d8568c4a9b23708
venuras <venuras@codegen.net>
2024-12-04 14:27:16 +0530

    modified code to run with test configuration. Useful when executing testcontainers

--- web-backend/src/test/java/com/sun_travels/web_backend/WebBackendApplicationTests.java
+++ web-backend/src/test/java/com/sun_travels/web_backend/WebBackendApplicationTests.java
@@ -1,13 +1,21 @@
 package com.sun_travels.web_backend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ContextConfiguration
+@ActiveProfiles(value = "test")
 class WebBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//Test to execute application and check whether it runs with the current configuration and resources
 	}
 
 }
