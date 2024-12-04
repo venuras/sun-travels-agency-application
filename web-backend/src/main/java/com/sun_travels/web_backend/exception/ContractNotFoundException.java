14e5b9d6bff3cbcde0f2c660da8f4ffc39ff4e18
venuras <venuras@codegen.net>
2024-12-04 14:22:36 +0530

    exception to throw when a contract is not found

---
+++ web-backend/src/main/java/com/sun_travels/web_backend/exception/ContractNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sun_travels.web_backend.exception;
+
+public class ContractNotFoundException extends RuntimeException {
+    public ContractNotFoundException(String message) {
+        super(message);
+    }
+}
