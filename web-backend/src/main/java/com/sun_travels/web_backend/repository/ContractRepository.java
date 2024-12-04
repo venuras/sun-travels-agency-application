0619affca96599396bff57d2c903a592083d1e45
venuras <venuras@codegen.net>
2024-12-04 17:16:10 +0530

    added repository annotation

--- web-backend/src/main/java/com/sun_travels/web_backend/repository/ContractRepository.java
+++ web-backend/src/main/java/com/sun_travels/web_backend/repository/ContractRepository.java
@@ -2,6 +2,8 @@ package com.sun_travels.web_backend.repository;
 
 import com.sun_travels.web_backend.model.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContractRepository extends JpaRepository<Contract, Long> {
 }
