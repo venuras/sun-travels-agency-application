bb9aa9b44e990b1c13aefa905bb8887d012e198d
venuras <venuras@codegen.net>
2024-12-04 14:23:19 +0530

    new contract room dto

---
+++ web-backend/src/main/java/com/sun_travels/web_backend/dto/ContractRoomDto.java
@@ -0,0 +1,21 @@
+package com.sun_travels.web_backend.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Valid
+public class ContractRoomDto {
+    private Long id;
+
+    @NotBlank
+    private String roomType;
+    private double roomPrice;
+    private int roomCount;
+    private int maxAdultCount;
+}
