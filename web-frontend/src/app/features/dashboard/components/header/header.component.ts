7a3acb499063e4c8c8085d0512e99aa0a19966a5
venuras <venuras@codegen.net>
2024-12-10 01:14:47 +0530

    used NgOptimizedImage

--- web-frontend/src/app/features/dashboard/components/header/header.component.ts
+++ web-frontend/src/app/features/dashboard/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
-
+import { NgOptimizedImage } from "@angular/common";
 @Component({
     selector:'feature-dashboard-header',
-    templateUrl:'./header.component.html'
+    templateUrl:'./header.component.html',
+    styleUrl: './header.component.css',
+    imports: [NgOptimizedImage]
 })
 export class HeaderComponent{
-
+    logoUrl: string = "sun-travels-logo.jpg"
 }
