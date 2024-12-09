32cb902b2c4ff27295d5612c6947496923a05024
venuras <venuras@codegen.net>
2024-12-10 01:15:49 +0530

    modified sidebar with styles

--- web-frontend/src/app/features/dashboard/components/sidebar/sidebar.component.ts
+++ web-frontend/src/app/features/dashboard/components/sidebar/sidebar.component.ts
@@ -1,7 +1,16 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'feature-dashboard-sidebar',
-    templateUrl: './sidebar.component.html'
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.css'],
+    imports: [RouterLink]
 })
-export class SidebarComponent{}
+export class SidebarComponent {
+    selectedButton: string = 'searchHotels';
+
+    handleClick(button: string) {
+        this.selectedButton = button;
+    }
+}
