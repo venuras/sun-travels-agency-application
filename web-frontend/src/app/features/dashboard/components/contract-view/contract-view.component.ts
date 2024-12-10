e1a23ebb36d54718ea3ed32fe715800f38744235
venuras <venuras@codegen.net>
2024-12-10 11:20:22 +0530

    imported styles and created functionality to fetch contracts on init

--- web-frontend/src/app/features/dashboard/components/contract-view/contract-view.component.ts
+++ web-frontend/src/app/features/dashboard/components/contract-view/contract-view.component.ts
@@ -1,7 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
+import { ApiService } from "../../../../core/service/api.service";
+import { ContractService } from "../../services/contract.service";
+import { Observable } from "rxjs";
+import { Contract } from "../../../../shared/types/contract.type";
 
 @Component({
     selector:'feature-dashboard-search-view',
-    templateUrl:'./contract-view.component.html'
+    templateUrl:'./contract-view.component.html',
+    styleUrl: './contract-view.component.css',
 })
-export class ContractViewComponent{}
+export class ContractViewComponent implements OnInit{
+    ngOnInit(): void {
+        this.getContracts();
+    }
+
+    contractService:ContractService = inject(ContractService);
+
+    getContracts():void {
+        let contracts$ = this.contractService.getContracts();
+        contracts$.subscribe((value) => {
+            console.log(value);
+        });
+    }
+}
