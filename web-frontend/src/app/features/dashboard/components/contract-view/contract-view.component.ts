96d94098239297fe50eed5ba6b7dc54a5e0269bc
venuras <venuras@codegen.net>
2024-12-13 14:06:16 +0530

    reflecting changes occurred due to add contract modal

--- web-frontend/src/app/features/dashboard/components/contract-view/contract-view.component.ts
+++ web-frontend/src/app/features/dashboard/components/contract-view/contract-view.component.ts
@@ -4,28 +4,44 @@ import { ContractService } from "../../services/contract.service";
 import { catchError, ignoreElements, Observable, of } from "rxjs";
 import { Contract } from "../../../../shared/types/contract.type";
 import { ContractDetailsModalComponent } from "../contract-details-modal/contract-details-modal.component";
+import { AddContractModalComponent } from "../add-contract-modal/add-contract-modal.component";
 
 @Component({
     selector: 'feature-dashboard-search-view',
     templateUrl: './contract-view.component.html',
     styleUrl: './contract-view.component.css',
-    imports: [ContractDetailsModalComponent]
+    imports: [ContractDetailsModalComponent, AddContractModalComponent]
 })
 export class ContractViewComponent implements OnInit {
+    handleAddContract(contract: Contract) {
+        let savedContract$:Observable<Contract> = this.contractService.createContract(contract);
+        savedContract$.pipe(catchError((err) => {
+            console.error(err);
+            alert('An unexpected error occurred!');
+            return of();
+        })).subscribe((savedContract) => this.contracts.push(savedContract));
+    }
+    isAddContractModalOpen: boolean = false;
+    closeAddContractModal() {
+        this.isAddContractModalOpen = false;
+    }
+    openAddContractModal() {
+        this.isAddContractModalOpen = true;
+    }
 
-    selectedContract?:Contract;
+    selectedContract?: Contract;
     closeRoomDetailsModal() {
         this.isModalOpen = false;
     }
 
-    openRoomDetailsModal(conntract:Contract) {
+    openRoomDetailsModal(conntract: Contract) {
         this.selectedContract = conntract;
         this.isModalOpen = true;
     }
 
     contracts: Contract[] = [];
 
-    isModalOpen:boolean = false;
+    isModalOpen: boolean = false;
 
     ngOnInit(): void {
         this.getContracts();
